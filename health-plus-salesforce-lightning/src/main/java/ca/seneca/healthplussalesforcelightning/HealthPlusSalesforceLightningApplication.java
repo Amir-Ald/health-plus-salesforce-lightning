d8659868cf7e497d0ad1e6a2c29e344756a7be12
Amir-Ald <amirreza4747@gmail.com>
2024-10-08 20:48:55 -0400

    initial commit

---
+++ health-plus-salesforce-lightning/src/main/java/ca/seneca/healthplussalesforcelightning/HealthPlusSalesforceLightningApplication.java
@@ -0,0 +1,13 @@
+package ca.seneca.healthplussalesforcelightning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthPlusSalesforceLightningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthPlusSalesforceLightningApplication.class, args);
+	}
+
+}
---
+++ health-plus-salesforce-lightning/src/test/java/ca/seneca/healthplussalesforcelightning/HealthPlusSalesforceLightningApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.seneca.healthplussalesforcelightning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthPlusSalesforceLightningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
